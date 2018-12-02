bb75e317fa572b2cf43103706990666e415497c5
Andrew Zirm <andrewzirm@greenhouse.io>
2018-12-01 21:08:28 -0500

    Day 01

--- src/aoc_2018_cljs/day01.cljs
+++ src/aoc_2018_cljs/day01.cljs
@@ -1,4 +1,22 @@
 (ns aoc-2018-cljs.day01
   (:require [aoc-2018-cljs.util :refer [slurp-resource line-seq-resource]]))
 
-(+ 1 1)
+(def freq-changes (->> "day01.txt"
+                       line-seq-resource
+                       (map #(js/parseInt %))))
+
+(defn find-first-repeat
+  [freq-changes]
+  (let [redux (reductions + 0 (cycle freq-changes))]
+    (reduce (fn [seen freq]
+                (if (seen freq)
+                  (reduced freq)
+                  (conj seen freq)))
+      #{}
+      redux)))
+
+; part 1
+(reduce + freq-changes)
+
+; part 2
+(find-first-repeat freq-changes)
