87f33b34e37699da272d952f1cf8ff87a6bcd2d1
Andrew Zirm <andrewzirm@greenhouse.io>
2018-12-07 22:56:09 -0500

    day04

--- src/aoc_2018_cljs/day04.cljs
+++ src/aoc_2018_cljs/day04.cljs
@@ -12,7 +12,7 @@
   (re-pattern "Guard #(\\d+)\\s"))
 
 (defn action-matcher []
-  (re-pattern "falls asleep|wakes up"))
+  (re-pattern "falls asleep|wakes up|begins shift"))
 
 (def guard-atom (atom 0))
 (def action-atom (atom :awake))
@@ -23,6 +23,7 @@
         guard (reset! guard-atom (or guard-str @guard-atom))
         action-string (re-find (action-matcher) code)
         action (case action-string
+                     "begins shift" (reset! action-atom :begins)
                      "falls asleep" (reset! action-atom :asleep)
                      "wakes up" (reset! action-atom :awake)
                      @action-atom)]
@@ -30,17 +31,33 @@
                :hour (js/parseInt hour) :minute (js/parseInt minute)
                :guard (js/parseInt guard) :action action)))
 
+(defn final [codes]
+  (let [parsed (map parse-line codes)
+        sorted (map #(parse-line (:code %)) (sort-by (juxt :year :month :day :hour :minute) parsed))
+        filtered (filter #(or (= (:action %) :asleep) (= (:action %) :awake)) sorted)]
+    (map #(parse-line (:code %)) filtered)))
+
+(defn sum-minutes [events]
+  (reduce + (map
+               #(- (:minute (nth % 1)) (:minute (nth % 0))) (partition 2 events))))
+
+(defn minutes-range [events]
+  (conj '() (map #(range (:minute (nth % 0)) (- (:minute (nth % 1)) 0) 1) (partition 2 events))))
+
+(defn total-by-guard [codes]
+  (let [all-good (final codes)
+        grouped (group-by (juxt :year :month :day :guard) all-good)
+        total-by-day-guard (map #(assoc {} :group % :guard (:guard (nth (get grouped %) 0)) :minute-sum (sum-minutes (get grouped %)) :minutes (minutes-range (get grouped %))) (keys grouped))]
+    (group-by :guard total-by-day-guard)))
+
+(defn max-id [totals]
+  (map #(assoc {} :id % :total (reduce + (map :minute-sum (get totals %)))) (keys totals)))
+
+(defn max-minute [totals]
+  (map #(assoc {} :id % :freqs (frequencies (flatten (reduce conj '() (map :minutes (get totals %)))))) (keys totals)))
+
 (comment
   (def code1 (first codes))
-  (parse-date code1)
-
-  (def maps
-    (map #(assoc {}
-                 :code %
-                 :date (parse-date %)
-                 :hour (parse-hour %)
-                 :minute-ten (first (parse-minute %))
-                 :minute-one (second (parse-minute %))) codes))
-
-  (def sorted (sort-by :date maps))
-)
+  (def t-by-g (total-by-guard codes))
+  (def mid (max-id t-by-g))
+  (def max-min (max-minute t-by-g)))
