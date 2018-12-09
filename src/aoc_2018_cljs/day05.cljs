f6e17cd58aec4f74c7abcea13aeb0d6a6ec904e5
Andrew Zirm <andrewzirm@gmail.com>
2018-12-09 14:04:54 -0500

    finished day05

--- src/aoc_2018_cljs/day05.cljs
+++ src/aoc_2018_cljs/day05.cljs
@@ -2,26 +2,41 @@
   (:require [aoc-2018-cljs.util :refer [to-int cart slurp-resource line-seq-resource]]
             [clojure.string :as string]))
 
-(def codes (->> "day05.txt"
-                line-seq-resource))
+(def polymer (first (->> "day05.txt"
+                       line-seq-resource)))
 
-(defn react [_ a b]
-  (let [a-up-b-low (if
-                     (and
-                      (not= (string/lower-case a) a)
-                      (not= (string/upper-case b) b)
-                      (= (string/lower-case a) (string/lower-case b))) true false)
-        a-low-b-up (if
-                     (and
-                      (not= (string/lower-case b) b)
-                      (not= (string/upper-case a) a)
-                      (= (string/lower-case a) (string/lower-case b))) true false)]
-    (if (or a-up-b-low a-low-b-up)
-      ""
-      (str a b))))
+(defn react? [a b]
+ (and
+    (= (string/lower-case b) (string/lower-case a))
+    (not= a b)))
+
+(defn build-chain [polymer unit]
+  (if (some->
+       (peek polymer)
+       (react? unit))
+    (pop polymer)
+    (conj polymer unit)))
+
+(defn react [polymer]
+  (reduce build-chain [] polymer))
+
+(def alphabet "abcdefghijklmnopqrstuvwxyz")
+
+(defn remove-and-build [polymer bad-unit]
+  (let [bad-re (re-pattern (str "(?i)" bad-unit))
+        new-polymer (string/replace polymer bad-re "")]
+    {:unit (keyword bad-unit) :count (count (react new-polymer))}))
+
+(def all-units (map #(remove-and-build polymer %) alphabet))
+
+(defn part-1 [polymer]
+  (count (react polymer)))
+
+(defn part-2 [polymer]
+  (let [all-units (map #(remove-and-build polymer %) alphabet)]
+    (:count (first (sort-by :count all-units)))))
 
 (comment
   (def re-pair (re-pattern "(.)(.)"))
   (def str1 "XxSsdDIijNnJACszZScZfFhHQyYrRqzdXTtxDEeNnGgOaAcCMcCL")
-  (string/replace str1 re-pair "b")
-)
+  (string/replace str1 re-pair "b"))
